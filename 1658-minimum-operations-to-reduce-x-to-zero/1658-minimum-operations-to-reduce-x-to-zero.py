257ad56e47e9d74e8c1609431494e1588d9f989d
ashutosh kumar <ashutosh.kumar.jha258@gmail.com>
2026-09-23 22:09:40 +0530

    Time: 92 ms (46.14%), Space: 30.7 MB (93.25%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,25 +1,17 @@
 class Solution:
     def minOperations(self, nums: list[int], x: int) -> int:
-        n = len(nums)
-        prefix = {0: 0}
-        suffix = []
-        ps = 0
-        ss = 0
-        for i in range(n):
-            ps += nums[i]
-            if ps > x:
-                break
-            prefix[ps] = i + 1
-        for i in range(n - 1, -1, -1):
-            ss += nums[i]
-            if ss > x:
-                break
-            suffix.append((ss, n - i))
-        ans = prefix.get(x, n + 1)
-        for suffix_sum, suffix_count in suffix:
-            needed = x - suffix_sum
-            if needed in prefix:
-                prefix_count = prefix[needed]
-                if prefix_count + suffix_count <= n:
-                    ans = min(ans, prefix_count + suffix_count)
-        return -1 if ans == n + 1 else ans
+        l=0
+        max_length=-1
+        running_sum=0
+        target=sum(nums)-x
+        for r in range(len(nums)):
+            running_sum+=nums[r]
+            while l<=r and running_sum>target:
+                running_sum-=nums[l]
+                l+=1
+            if(running_sum==target):
+                max_length=max(max_length,r-l+1)
+        if max_length==-1:
+            return -1 
+        else:
+            return len(nums)-max_length
